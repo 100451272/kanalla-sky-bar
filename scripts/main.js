7e1b18f064dfa93d93f592ffb9450d666269d505
Manu Alonso <100451272@alumnos.uc3m.es>
2024-10-18 13:43:09 +0200

    Added general menu and take away text

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,14 @@ function goBurger() {
 function goBrunch() {
   document.location.href = "kanalla-brunch.html";
 }
+
+window.onload = function() {
+  if (document.location.href.includes("kanalla-burger") && new Date().getDay() <= 22) {
+    document.getElementsByClassName("popup")[0].style.opacity = 100;
+  }
+}
+
+function closePopup() {
+  document.getElementsByClassName("popup")[0].style.opacity = 0;
+  document.getElementsByClassName("popup")[0].style.display = "none";
+}
